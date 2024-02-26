f8c4cdf043cdb3711928c2a5d77fd103992088d3
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-02-26 22:44:14 +0000

    echo function implemented.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Telegram Bot, Test Commit")
+from aiogram import Bot, Dispatcher, types
+
+TOKEN_API = "6853415335:AAFuL2FRe1vy9-5jGLJJoCMkEu5OcxSB4iU"
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher()
+
+@dp.message()
+async def echo(message: types.Message): #give answer as well as you type.
+    await message.answer(message.text)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
+
+
