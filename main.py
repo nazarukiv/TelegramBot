22f99cc7f3e940904d7151c3ca56813b2ccdd920
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-02-27 15:08:06 +0000

    Keyboard commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher, types
 import string
 import random
-
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from aiogram.filters import Command
 
 TOKEN_API = "6853415335:AAFuL2FRe1vy9-5jGLJJoCMkEu5OcxSB4iU" #token to verify telegram API
@@ -9,26 +9,53 @@ TOKEN_API = "6853415335:AAFuL2FRe1vy9-5jGLJJoCMkEu5OcxSB4iU" #token to verify te
 bot = Bot(TOKEN_API)
 dp = Dispatcher()
 
-count = 0
+keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='/help')],
+        [KeyboardButton(text='/give')],
+        [KeyboardButton(text='/start')],
+        [KeyboardButton(text='/description')],
+        [KeyboardButton(text='/photo')],
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True # default is False
+)
+
+
+HELP_COMMAND = """
+<b>/help</b>-<em>list of commands</em>
+<b>/give</b>-<em>send the picture of cat</em>
+<b>/start</b>-<em>start the bot</em>
+<b>/description</b>-<em>description of bot</em>
+<b>/photo</b>-<em>photo</em>"""
 
 @dp.message(Command("start")) #handle
 async def start_command(message: types.Message): #parse_mode allow to add html tags
-    await message.answer('<em>Hello, <b>welcome to my bot</b>, done by Ivan Nazaruk!</em>', parse_mode="HTML")
+    await bot.send_message(message.from_user.id,
+                         text='<em>Hello, <b>welcome to my bot</b>, done by Ivan Nazaruk!</em>',
+                         parse_mode="HTML",
+                        reply_markup=keyboard)
+    await message.delete()
 
+@dp.message(Command("help"))
+async def help_command(message: types.Message):
+    await bot.send_message(message.from_user.id,
+                           text=HELP_COMMAND,
+                           parse_mode="HTML")
+    await message.delete()
 
-@dp.message()
-async def check_for_heart(message: types.Message):
-    global count
-    text = message.text  # the text of the message
-    heart_count = text.count("<")
+@dp.message(Command("description"))
+async def description_command(message: types.Message):
+    await bot.send_message(message.from_user.id,
+                           text="Our bot know how to send a photo",
+                           parse_mode="HTML")
+    await message.delete()
 
-    if heart_count > 0:
-        count += heart_count  # update the global count
-        await message.answer(f"Count of '<': {heart_count}")
-    else:
-        await message.answer("No <")
-
-
+@dp.message(Command("photo"))
+async def photo_command(message: types.Message):
+    await bot.send_photo(message.from_user.id,
+                         photo="https://m.media-amazon.com/images/I/81AheVnqHXL._AC_UF894,1000_QL80_.jpg")
+    await message.delete()
 
 async def main():
     await dp.start_polling(bot)
@@ -36,5 +63,4 @@ async def main():
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
-
 
