42f5b8dffb267e41dae7326b9c4bbcd5b68667d8
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-02-27 15:57:13 +0000

    Inline Keyboard commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Dispatcher, types
 import string
 import random
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.filters import Command
 
 TOKEN_API = "6853415335:AAFuL2FRe1vy9-5jGLJJoCMkEu5OcxSB4iU" #token to verify telegram API
@@ -11,16 +12,20 @@ dp = Dispatcher()
 
 keyboard = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text='/help')],
+        [KeyboardButton(text='/start'), KeyboardButton(text='/help')],
+        [KeyboardButton(text='/description'), KeyboardButton(text='/photo')],
         [KeyboardButton(text='/give')],
-        [KeyboardButton(text='/start')],
-        [KeyboardButton(text='/description')],
-        [KeyboardButton(text='/photo')],
     ],
     resize_keyboard=True,
-    one_time_keyboard=True # default is False
+    one_time_keyboard=True  # making the keyboard disappear after a button is pressed
 )
 
+
+
+keyboard_inline = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Button 1', url="https://www.youtube.com/@SerhiiNemchynskyi")],
+    [InlineKeyboardButton(text='Button 2', url="https://www.youtube.com/@sbermarket_tech")],
+])
 
 HELP_COMMAND = """
 <b>/help</b>-<em>list of commands</em>
@@ -32,9 +37,8 @@ HELP_COMMAND = """
 @dp.message(Command("start")) #handle
 async def start_command(message: types.Message): #parse_mode allow to add html tags
     await bot.send_message(message.from_user.id,
-                         text='<em>Hello, <b>welcome to my bot</b>, done by Ivan Nazaruk!</em>',
-                         parse_mode="HTML",
-                        reply_markup=keyboard)
+                         text='Hello World',
+                        reply_markup=keyboard_inline)
     await message.delete()
 
 @dp.message(Command("help"))
