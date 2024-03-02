5aa05afd9e7377e1f814374c09ea6bf5b6597a27
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-03-02 18:27:58 +0000

    Commit with random value and inline keyboard counter.

--- main.py
+++ main.py
@@ -3,31 +3,46 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.filters import Command
 import asyncio
+import random
 
 TOKEN_API = "6853415335:AAFuL2FRe1vy9-5jGLJJoCMkEu5OcxSB4iU"  # Token to verify Telegram API
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher()
 
+number = 1000
+
 keyboard_inline = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='❤️', callback_data="like"),
      InlineKeyboardButton(text='👎', callback_data="dislike")],
 ])
 
+def get_inline_keyboard() -> InlineKeyboardMarkup:
+    ikb = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Random number', callback_data="btn_random"),
+     InlineKeyboardButton(text='1000', callback_data="btn_hun")],
+])
+    return ikb
+
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message) -> None:
-    await bot.send_photo(chat_id=message.from_user.id,
-                         photo="https://www.muchbetteradventures.com/magazine/content/images/size/w2000/2019/10/29122602/iStock-971053374.jpg",
-                         caption="Do you like this photo?",
-                         reply_markup=keyboard_inline)
+    await message.answer(f"The current number is {number}",
+                         reply_markup=get_inline_keyboard())
 
 @dp.callback_query()
-async def ikb_cb_handler(callback: types.CallbackQuery):
-    print(callback)
-    if callback.data == 'like':
-        await callback.answer("You like it!That's cool")
+async def ikb_callback(callback: types.CallbackQuery) -> None:
+    global number
+    if callback.data == "btn_random":
+        await callback.message.edit_text(f"The current number is {random.randint(1,10)}",
+                                         reply_markup=get_inline_keyboard())
+    elif callback.data == "btn_hun":
+        await callback.message.edit_text(f"The current number is {number}",
+                                         reply_markup=get_inline_keyboard())
     else:
-        await callback.answer("You don't like it :(")
+        1/0
+
+
+
 
 async def main():
     await dp.start_polling(bot)
